857594d2565fa5d20e0daf866b217c95903d0343
User Name <landonisaiah@gmail.com>
2025-10-19 00:19:36 -0700

    add get single artwork postman test success

--- artworks/urls.py
+++ artworks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArtworkListView
+from .views import ArtworkListView, ArtworkDetailView
 
 urlpatterns = [
     path('', ArtworkListView.as_view()),
+    path('<int:pk>/', ArtworkDetailView.as_view()),
 ]
--- artworks/views.py
+++ artworks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Artwork
@@ -34,4 +35,21 @@ class ArtworkListView(APIView):
                 e.__dict__ if e.__dict__ else str(e),
                 status=status.HTTP_422_UNPROCESSABLE_ENTITY
             )
-            
+
+class ArtworkDetailView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_artwork(self, pk):
+        try:
+            return Artwork.objects.get(pk=pk)
+        except Artwork.DoesNotExist:
+            raise NotFound(detail="Can't find that artwork")
+
+    def get(self, _request, pk):
+        artwork = self.get_artwork(pk=pk)
+        serialized_artwork = ArtworkSerializer(artwork)
+        return Response(serialized_artwork.data, status=status.HTTP_200_OK)
+
+
+
+
