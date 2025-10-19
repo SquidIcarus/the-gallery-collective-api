8e0b5531d41985c020709f682a6b33cec263f735
User Name <landonisaiah@gmail.com>
2025-10-18 23:52:35 -0700

    add post method to upload artwork

--- artworks/views.py
+++ artworks/views.py
@@ -14,3 +14,24 @@ class ArtworkListView(APIView):
         artworks = Artwork.objects.all()
         serialized_artworks = ArtworkSerializer(artworks, many=True)
         return Response(serialized_artworks.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        if not hasattr(request.user, 'artist'):
+            return Response(
+                {'error': 'Only artists can upload artwork'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        request.data['artist'] = request.user.artist.user_id
+
+        artwork_to_add = ArtworkSerializer(data=request.data)
+        try:
+            artwork_to_add.is_valid(raise_exception=True)
+            artwork_to_add.save()
+            return Response(artwork_to_add.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response(
+                e.__dict__ if e.__dict__ else str(e),
+                status=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+            
