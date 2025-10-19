b5d9c26813899acdfab5de03cb250a615da6439a
User Name <landonisaiah@gmail.com>
2025-10-18 17:11:00 -0700

    GET single artist, postman test success

--- artists/urls.py
+++ artists/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArtistListView
+from .views import ArtistListView, ArtistDetailView
 
 urlpatterns = [
     path('', ArtistListView.as_view()),
+    path('<int:pk>/', ArtistDetailView.as_view()),
 ]
 
--- artists/views.py
+++ artists/views.py
@@ -16,6 +16,22 @@ class ArtistListView(APIView):
         serialized_artists = ArtistSerializer(artists, many=True)
         return Response(serialized_artists.data, status=status.HTTP_200_OK)
 
+class ArtistDetailView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+    def get_artist(self, pk):
+        try:
+            return Artist.objects.get(pk=pk)
+        except Artist.DoesNotExist:
+            raise NotFound(detail="Can't find that artist")
+
+
+    def get(self, _request, pk):
+        artist = self.get_artist(pk=pk)
+        serialized_artist = ArtistSerializer(artist)
+        return Response(serialized_artist.data, status=status.HTTP_200_OK)
+
  
 
 
