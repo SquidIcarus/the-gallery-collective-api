e49bd919c9bdb9d77e614412903bc6543fdbc7b8
User Name <landonisaiah@gmail.com>
2025-10-22 23:31:28 -0700

    add medium to artworks model

--- artworks/models.py
+++ artworks/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
+
 class Artwork(models.Model):
     def __str__(self):
         return f'{self.title} by {self.artist.user.username}'
 
     title = models.CharField(max_length=200)
+    year_created = models.IntegerField()
+    medium = models.CharField(max_length=100)
+    dimensions = models.CharField(max_length=100, blank=True)
     description = models.TextField()
     image = models.ImageField(upload_to='artworks/')
-    dimensions = models.CharField(max_length=100, blank=True)
-    year_created = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     artist = models.ForeignKey(
         "artists.Artist",
